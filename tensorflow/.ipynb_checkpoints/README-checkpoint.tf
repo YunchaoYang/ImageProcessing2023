f049497ab5b8deb1eaed99731b8976d39c2f4184
Yunchao Yang <hityangsir@ufl.edu>
2023-11-09 12:17:06 -0500

    first commit

---
+++ tensorflow/.ipynb_checkpoints/README-checkpoint.tf
@@ -0,0 +1,27 @@
+
+     Dimitri Bourilkov  7-Nov-2023
+
+      TensorFlow Image Processing
+
+1) Copy the files
+
+convnet_cifar_conf1.py
+
+and
+
+convnet_cifar_conf5.py
+
+to your working directory (preferably on the /blue filesystyem).
+
+2) Load TensorFlow
+
+ml purge
+ml ngc-tensorflow/2.4.0
+ml
+
+3) Run the examples
+
+time python convnet_cifar_conf1.py
+
+time python convnet_cifar_conf5.py
+
---
+++ tensorflow/EXERCISE.tf
@@ -0,0 +1,21 @@
+
+     Dimitri Bourilkov  7-Nov-2023
+
+        TensorFlow Exercise
+
+1) Copy the code 
+
+convnet_cifar_conf5.py
+
+to
+
+convnet_cifar_conf5_tanh.py
+
+2) Replace the activation functions in all layers:
+
+relu -> tanh
+
+3) Run again the code and compare the results:
+
+time python convnet_cifar_conf5_tanh.py
+
